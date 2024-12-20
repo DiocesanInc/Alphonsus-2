b43071bd74353720e622bc7f309ea12df6b638e3
spohlman <spohlman@diocesan.com>
2024-12-20 15:01:56 -0500

    parish cluster buttons

--- template-parts/homepage/parishes.php
+++ template-parts/homepage/parishes.php
@@ -9,20 +9,21 @@
 
  ?>
  <div class="parish-cluster">
-    <?php if(get_field('parish_scroll_title'));?> 
-    <h2 class="parish-scroll-title has-text-decoration text-decoration-is-centered has-primary-background-color-after">
-        <?php echo get_field('parish_scroll_title');?>
-    </h2>
+    <?php if(get_field('parish_scroll_title')):?> 
+        <h2 class="parish-scroll-title has-text-decoration text-decoration-is-centered has-primary-background-color-after">
+            <?php echo get_field('parish_scroll_title');?>
+        </h2>
+    <?php endif;?>
     <?php if (have_rows('parish_buttons')&&(get_field('cluster_style') == 'buttons')) : ?>
-    <div class="featured-buttons parishBtns" data-btns="<?php echo count(get_field('parish_buttons')); ?>">
-        <div class="featured-buttons-wrapper parishButtons">
-            <?php while (have_rows('parish_buttons')) : the_row();
-                get_template_part("template-parts/components/parish", "button");
-            endwhile; ?>
+        <div class="featured-buttons parishBtns" data-btns="<?php echo count(get_field('parish_buttons')); ?>">
+            <div class="featured-buttons-wrapper parishButtons">
+                <?php while (have_rows('parish_buttons')) : the_row();
+                    get_template_part("template-parts/homepage/parish", "button");
+                endwhile; ?>
+            </div>
         </div>
-    </div>
- <?php elseif(have_rows('parish_carousel')&&(get_field('cluster_style') == 'carousel')) : 
-  get_template_part("template-parts/homepage/parish", "carousel");?>
- <?php endif;?>
+    <?php elseif(have_rows('parish_carousel')&&(get_field('cluster_style') == 'carousel')) : 
+        get_template_part("template-parts/homepage/parish", "carousel");?>
+    <?php endif;?>
  
  </div>
